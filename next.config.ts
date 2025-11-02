639e61a7e648f0054ead5e95a7745d3727fa75ef
onyourjie <filzahmufidah@student.ub.ac.id>
2025-11-02 21:49:26 +0700

    fix : deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
